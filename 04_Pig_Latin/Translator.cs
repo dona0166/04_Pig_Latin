591b439755623bdf1c4bc882c3fa8735dde03ad7
Donnie <Donnie@DESKTOP-A0TIJCR>
2016-09-20 18:26:08 +0200

    qu..qu qu ay.....quay...quasi

--- 04_Pig_Latin/Translator.cs
+++ 04_Pig_Latin/Translator.cs
@@ -1,10 +1,43 @@
 ﻿namespace _04_Pig_Latin
 {
-    public class Translator
+    internal class Translator
     {
-        public Translator()
+        internal string Translate(string something)
         {
-
+            int a = 0;
+            
+            
+            string result = " ";
+            string[] anarray = something.Split(' ');
+            foreach(string astring in anarray)
+            {   
+                while((astring.Substring(a, 1) != "a")&&(astring.Substring(a, 1)!= "e")&&(astring.Substring(a, 1) != "i")
+                    &&(astring.Substring(a, 1) != "o")&&(astring.Substring(a, 1) != "u")){
+                    a++;
+                    
+                
+                }
+                if (a == 0)
+                {
+                    result += astring + "ay ";
+                    a = 0;
+                }
+                else 
+                {
+                    if(astring.Substring(a-1,2) == "qu")
+                    {
+                        result += astring.Substring(a + 1) + astring.Substring(0, a + 1) + "ay ";
+                        a = 0;
+                    }
+                    else
+                    {
+                        result += astring.Substring(a) + astring.Substring(0, a) + "ay ";
+                        a = 0;
+                    }
+                    
+                }
+             }
+                return result.Trim();
         }
     }
 }
